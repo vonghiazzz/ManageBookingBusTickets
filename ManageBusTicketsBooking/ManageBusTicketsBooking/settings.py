d0490f647cf07fe1b8ecb29f9b7bdbbacf382edb
VoTanNghia <117810261+vonghiazzz@users.noreply.github.com>
2024-09-23 21:44:20 +0700

    Fix setting

--- ManageBusTicketsBooking/ManageBusTicketsBooking/settings.py
+++ ManageBusTicketsBooking/ManageBusTicketsBooking/settings.py
@@ -202,5 +202,17 @@ load_dotenv()
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('GOOGLE_OAUTH2_SECRET')
 
+# Cài đặt pipeline cho social-auth
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',  
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 import django_heroku
 django_heroku.settings(locals())
