24ffbea73f2721aaebe3bbb164b2d671707b8389
VoTanNghia <117810261+vonghiazzz@users.noreply.github.com>
2024-10-01 22:08:55 +0700

    Update create customer function

--- ManageBusTicketsBooking/ManageBusTicketsBooking/settings.py
+++ ManageBusTicketsBooking/ManageBusTicketsBooking/settings.py
@@ -231,7 +231,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',  # Tạo User mới
-    'path.to.your.create_customer',  # Thêm khách hàng sau khi tạo User
+    'app.models.create_customer',  # Thêm khách hàng sau khi tạo User
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
